82253a20e524b6a4cd93552b837e86ff0c59659a
omkar-verekar <omkar_verekar@persistent.com>
2019-08-21 14:34:41 +0530

    fix defect i to we

--- App.java
+++ App.java
@@ -2,6 +2,6 @@ public class App
 {
     public static void main(String args[])
     {
-        System.out.println("Hello world java i am awesome");
+        System.out.println("Hello world java we are awesome");
     }
 }
