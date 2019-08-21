06796fd82502f25dcbe77e78382fd8f2c8ffcbef
omkar-verekar <omkar_verekar@persistent.com>
2019-08-21 14:38:48 +0530

    i am not bored

--- App.java
+++ App.java
@@ -3,5 +3,6 @@ public class App
     public static void main(String args[])
     {
         System.out.println("Hello world java i am awesome");
+        System.out.println("i am not bored with training");
     }
 }
