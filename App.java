c1fc320eadef37243a284cb08938702527049b46
omkar-verekar <omkar_verekar@persistent.com>
2019-08-21 15:18:41 +0530

    testing 123

--- App.java
+++ App.java
@@ -3,7 +3,6 @@ public class App
     public static void main(String args[])
     {
 
-        System.out.println("Hello world java we are awesome");
-        System.out.println("i am not bored with training");
+        System.out.println("Testing 123");
     }
 }
